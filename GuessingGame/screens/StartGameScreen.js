7b237dcb02a1e5630e878638e8e45db73d0970f2
Timothy Karani <kartimothy@gmail.com>
2024-05-02 00:28:40 +0300

    49-Creating custom buttons

--- GuessingGame/screens/StartGameScreen.js
+++ GuessingGame/screens/StartGameScreen.js
@@ -1,10 +1,12 @@
-import { Text, TextInput, View } from "react-native";
+import { TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 export default function StartGameScreen() {
   return (
     <View>
       <TextInput />
-      <Text>BUTTONS</Text>
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
