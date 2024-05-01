e6813b9735125fceb778cba3f4fdf1f7ae3a865f
Timothy Karani <kartimothy@gmail.com>
2024-05-02 01:11:11 +0300

    52-adding visual feedback to buttons

--- GuessingGame/components/PrimaryButton.js
+++ GuessingGame/components/PrimaryButton.js
@@ -1,9 +1,41 @@
-import { Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function PrimaryButton({ children }) {
+  function pressHandler() {}
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.pressed, , styles.buttonInnerContainer]
+            : styles.buttonInnerContainer
+        }
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    borderRadius: 28,
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
