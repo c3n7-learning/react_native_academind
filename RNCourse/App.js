8c7cf374f6d51927a5795a2cc4e808b019881cd7
Timothy Karani <kartimothy@gmail.com>
2024-05-01 13:09:46 +0300

    16-styling react native apps

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -5,9 +5,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Another piece of text</Text>
+        <Text style={styles.dummyText}>Another piece of text</Text>
       </View>
-      <Text>Hello, World!</Text>
+      <Text style={styles.dummyText}>Hello, World!</Text>
       <Button title="Tap Me!" />
     </View>
   );
@@ -19,5 +19,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  dummyText: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: "#ff0",
+    padding: 16,
   },
 });
