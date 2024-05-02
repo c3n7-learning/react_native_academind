c9094da30531c656de94b07a8135fe3cb81d16b8
Timothy Karani <kartimothy@gmail.com>
2024-05-02 12:51:05 +0300

    63-Created a title component

---
+++ GuessingGame/components/Title.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text } from "react-native";
+
+export default function Title({ children }) {
+  return <Text style={styles.title}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#ddb52f",
+    textAlign: "center",
+    borderWidth: 2,
+    borderRadius: 4,
+    borderColor: "#ddb52f",
+    padding: 12,
+  },
+});
--- GuessingGame/screens/GameScreen.js
+++ GuessingGame/screens/GameScreen.js
@@ -1,9 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
+import Title from "../components/Title";
 
 export default function GameScreen() {
   return (
     <View style={styles.sceen}>
-      <Text>Opponent's Guess</Text>
+      <Title>Opponent's Guess</Title>
       <View>
         <Text>Higher or Lower?</Text>
       </View>
