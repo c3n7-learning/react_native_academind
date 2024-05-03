e3e310a280ba9bc195b91583340d4f159045f6ca
Timothy Karani <kartimothy@gmail.com>
2024-05-04 01:13:07 +0300

    103. Setting Navigation Options Dynamically

--- RNMeals/screens/MealsOverviewScreen.js
+++ RNMeals/screens/MealsOverviewScreen.js
@@ -1,13 +1,24 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
-import { MEALS } from "../data/dummy-data";
+import { FlatList, StyleSheet, View } from "react-native";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
+import { useLayoutEffect } from "react";
 
-export default function MealsOverviewScreen({ route }) {
+export default function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter(
     (mealItem) => mealItem.categoryIds.indexOf(catId) >= 0
   );
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
 
   function renderMealItem(itemData) {
     const item = itemData.item;
