504c7f48650d771f59a267ae9d3cdd45830b258c
Timothy Karani <kartimothy@gmail.com>
2024-05-04 23:19:58 +0300

    119. Managing Favorite Meals with the Context API

--- RNMeals/screens/FavoritesScreen.js
+++ RNMeals/screens/FavoritesScreen.js
@@ -1,5 +1,33 @@
-import { Text } from "react-native";
+import { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { FavoritesContext } from "../context/favorites-context";
+import { MEALS } from "../data/dummy-data";
+import MealsList from "../components/MealsList/MealsList";
 
 export default function FavoritesScreen() {
-  return <Text>My Favorites!</Text>;
+  const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealsCtx.ids.includes(meal.id)
+  );
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+    );
+  }
+  return <MealsList items={favoriteMeals} />;
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
