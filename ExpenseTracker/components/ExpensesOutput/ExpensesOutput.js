eb05ee640690d7e5ccaab5229186acbe9e9ce12b
Timothy Karani <kartimothy@gmail.com>
2024-05-06 23:49:11 +0300

    134. Working on Expense List Items

--- ExpenseTracker/components/ExpensesOutput/ExpensesOutput.js
+++ ExpenseTracker/components/ExpensesOutput/ExpensesOutput.js
@@ -8,31 +8,61 @@ const DUMMY_EXPENSES = [
     id: "e1",
     description: "A pair of shoes",
     amount: 89.99,
-    data: new Date("2023-11-19"),
+    date: new Date("2023-11-19"),
   },
   {
     id: "e2",
     description: "A pair of trousers",
     amount: 59.99,
-    data: new Date("2023-12-1"),
+    date: new Date("2023-12-1"),
   },
   {
     id: "e3",
     description: "Some bananas",
     amount: 5.99,
-    data: new Date("2023-12-5"),
+    date: new Date("2023-12-5"),
   },
   {
     id: "e4",
     description: "A Book",
     amount: 14.99,
-    data: new Date("2023-12-9"),
+    date: new Date("2023-12-9"),
   },
   {
     id: "e5",
     description: "Another Book",
     amount: 18.59,
-    data: new Date("2023-12-12"),
+    date: new Date("2023-12-12"),
+  },
+  {
+    id: "e6",
+    description: "A pair of shoes",
+    amount: 89.99,
+    date: new Date("2023-11-19"),
+  },
+  {
+    id: "e7",
+    description: "A pair of trousers",
+    amount: 59.99,
+    date: new Date("2023-12-1"),
+  },
+  {
+    id: "e8",
+    description: "Some bananas",
+    amount: 5.99,
+    date: new Date("2023-12-5"),
+  },
+  {
+    id: "e9",
+    description: "A Book",
+    amount: 14.99,
+    date: new Date("2023-12-9"),
+  },
+  {
+    id: "e10",
+    description: "Another Book",
+    amount: 18.59,
+    date: new Date("2023-12-12"),
   },
 ];
 
@@ -47,7 +77,9 @@ export default function ExpensesOutput({ expenses, expensesPeriod }) {
 
 const styles = StyleSheet.create({
   container: {
-    padding: 24,
+    paddingTop: 24,
+    paddingHorizontal: 24,
+    paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700,
     flex: 1,
   },
