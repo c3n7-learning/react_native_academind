bef40fb7829582e8f46154463deab50657dad23c
Timothy Karani <kartimothy@gmail.com>
2024-05-15 13:02:38 +0300

    135. Adding a Header Button & Making Expense Items Tappable

--- ExpenseTracker/components/ExpensesOutput/ExpenseItem.js
+++ ExpenseTracker/components/ExpensesOutput/ExpenseItem.js
@@ -3,36 +3,50 @@ import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
 
 export default function ExpenseItem({ description, date, amount }) {
+  function expensePressHandler() {}
+
   return (
-    <Pressable>
-      <View style={styles.expenseItem}>
-        <View>
-          <Text style={[styles.textBase, styles.description]}>
-            {description}
-          </Text>
-          <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
+    <View style={styles.expenseItemContainer}>
+      <Pressable
+        onPress={expensePressHandler}
+        android_ripple={{ color: GlobalStyles.colors.primary200 }}
+        style={({ pressed }) => pressed && styles.pressed}
+      >
+        <View style={styles.expenseItem}>
+          <View>
+            <Text style={[styles.textBase, styles.description]}>
+              {description}
+            </Text>
+            <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
+          </View>
+          <View style={styles.amountContainer}>
+            <Text style={styles.amount}>{amount.toFixed(2)}</Text>
+          </View>
         </View>
-        <View style={styles.amountContainer}>
-          <Text style={styles.amount}>{amount.toFixed(2)}</Text>
-        </View>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  expenseItem: {
-    padding: 12,
-    marginVertical: 8,
+  pressed: {
+    opacity: 0.75,
+  },
+  expenseItemContainer: {
     backgroundColor: GlobalStyles.colors.primary500,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    borderRadius: 6,
-    elevation: 3,
+    marginVertical: 8,
     shadowColor: GlobalStyles.colors.gray500,
     shadowRadius: 4,
     shadowOffset: { width: 1, height: 1 },
     shadowOpacity: 0.4,
+    elevation: 3,
+    borderRadius: 6,
+    overflow: "hidden",
+  },
+  expenseItem: {
+    padding: 12,
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   textBase: {
     color: GlobalStyles.colors.primary50,
