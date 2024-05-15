d7eb8090c874b6fa61c776ddb6bc3dfa8debd415
Timothy Karani <kartimothy@gmail.com>
2024-05-15 13:06:07 +0300

    136. Navigating Programmatically Between Screens

--- ExpenseTracker/components/ExpensesOutput/ExpenseItem.js
+++ ExpenseTracker/components/ExpensesOutput/ExpenseItem.js
@@ -1,9 +1,14 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
+import { useNavigation } from "@react-navigation/native";
 
 export default function ExpenseItem({ description, date, amount }) {
-  function expensePressHandler() {}
+  const navigation = useNavigation();
+
+  function expensePressHandler() {
+    navigation.navigate("ManageExpense");
+  }
 
   return (
     <View style={styles.expenseItemContainer}>
