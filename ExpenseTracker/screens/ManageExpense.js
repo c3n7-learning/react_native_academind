52e0f47fcd9c3739b68165a92e80619b3875dec2
Timothy Karani <kartimothy@gmail.com>
2024-05-15 13:20:28 +0300

    138. Supporting Different Editing Modes & Using Route Parameters

--- ExpenseTracker/screens/ManageExpense.js
+++ ExpenseTracker/screens/ManageExpense.js
@@ -1,5 +1,15 @@
+import { useLayoutEffect } from "react";
 import { Text } from "react-native";
 
-export default function ManageExpense() {
+export default function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
   return <Text>Manage Expenses</Text>;
 }
