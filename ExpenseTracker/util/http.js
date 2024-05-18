795181b22ce6d20e9a50b0208554a0b8d979fd70
Timothy Karani <kartimothy@gmail.com>
2024-05-18 16:12:46 +0300

    164. Fetching Backend Data (GET Requests)

--- ExpenseTracker/util/http.js
+++ ExpenseTracker/util/http.js
@@ -1,6 +1,24 @@
 import axios from "axios";
 import { FirebaseConfig } from "./firebase.config";
 
+const BACKEND_URL = FirebaseConfig.url;
+
 export function storeExpense(expenseData) {
-  axios.post(`${FirebaseConfig.url}/expenses.json`, expenseData);
+  axios.post(`${BACKEND_URL}/expenses.json`, expenseData);
+}
+
+export async function fetchExpenses() {
+  const response = axios.get(`${BACKEND_URL}/expense.json`);
+
+  const expenses = [];
+  for (const key in response.data) {
+    expenses.push({
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    });
+  }
+
+  return expenses;
 }
