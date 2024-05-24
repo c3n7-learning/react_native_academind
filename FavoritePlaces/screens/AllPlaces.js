fb4c03115e482e3f6814d5153ad1cacd68b2f3c2
Timothy Karani <kartimothy@gmail.com>
2024-05-24 23:39:53 +0300

    211. Outputting a List Of Places

--- FavoritePlaces/screens/AllPlaces.js
+++ FavoritePlaces/screens/AllPlaces.js
@@ -1,5 +1,16 @@
+import { useEffect, useState } from "react";
 import PlacesList from "../components/Places/PlacesList";
+import { useIsFocused } from "@react-navigation/native";
 
-export default function AllPlaces() {
-  return <PlacesList />;
+export default function AllPlaces({ route }) {
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused && route.params) {
+      setLoadedPlaces((current) => [...current, route.params.place]);
+    }
+  }, [route, isFocused]);
+
+  return <PlacesList places={loadedPlaces} />;
 }
